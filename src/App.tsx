fe6e3a813dac3c483c4f7351c4b1547bbdc50c92
michiharu <michi1diver@gmail.com>
2021-07-03 08:46:04 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Layer, Rect, Stage } from "react-konva";
 import { useWindowSize } from "./hooks";
 import Wave, { WaveParams, WaveProps } from "./Wave";
 
-const waveCount = 8;
+const waveCount = 6;
 const whiteRate = 0.7;
 const disappearRate = 0.76;
 const slowDownRate = 0.6;
@@ -11,11 +11,18 @@ const slowDownRate = 0.6;
 const randomRange = (range: number): number => -range + 2 * range * Math.random();
 
 const waveRandomParams = (am: number, tp: number): WaveParams => {
-  return [am + randomRange(am / 4), tp + randomRange(tp / 4), randomRange(Math.PI), randomRange(0.01)];
+  return [am + randomRange(am / 4), tp + randomRange(tp / 4), randomRange(Math.PI), randomRange(0.0005)];
 };
 
 const generateWaveParams = (): WaveParams[] => {
-  return [waveRandomParams(30, 600), waveRandomParams(20, 400), waveRandomParams(15, 200)];
+  return [
+    waveRandomParams(25, 610),
+    waveRandomParams(20, 470),
+    waveRandomParams(10, 230),
+    waveRandomParams(0.5, 29),
+    waveRandomParams(0.5, 23),
+    waveRandomParams(0.5, 19),
+  ];
 };
 
 const calcPoints = (width: number, line: number, endLine: number, params: WaveParams[]): number[] => {
@@ -24,10 +31,10 @@ const calcPoints = (width: number, line: number, endLine: number, params: WavePa
   const remain = line - slowDownLine;
   return [...Array(width + 1)].map((_, x) => {
     const y = params
-      .map(([am, tp, deg, speed]) => am * Math.sin((Math.PI / tp) * (deg + x) + line * speed))
+      .map(([am, tp, deg, speed]) => am * Math.sin((Math.PI / tp) * (deg + x) + line * am * speed))
       .reduce((a, b) => a + b);
     if (remain < 0) return y + line - 50;
-    return y + slowDownLine + remain * Math.cos((Math.PI * remain * 0.4) / maxRemain) - 50;
+    return y + slowDownLine + remain * Math.cos((Math.PI * remain * 0.45) / maxRemain) - 50;
   });
 };
 
@@ -44,7 +51,7 @@ const calcRGB = (a: RGB, b: RGB, rate: number): RGB => {
 type RGB = [number, number, number];
 const wave1: RGB = [0, 96, 161];
 const wave2: RGB = [102, 244, 255];
-const wave3: RGB = [150, 250, 255];
+const wave3: RGB = [200, 250, 255];
 const wave4: RGB = [132, 132, 132];
 
 const calcFill = (line: number, endLine: number): string => {
@@ -53,13 +60,14 @@ const calcFill = (line: number, endLine: number): string => {
     const [r, g, b] = calcRGB(wave1, wave2, rate / whiteRate);
     return `rgba(${r},${g},${b},1)`;
   }
+
   if (rate < disappearRate) {
     const [r, g, b] = calcRGB(wave2, wave3, (rate - whiteRate) / (disappearRate - whiteRate));
     return `rgba(${r},${g},${b},1)`;
   }
   const [r, g, b] = calcRGB(wave3, wave4, (rate - disappearRate) / (1 - disappearRate));
-  const opacity = rate < disappearRate ? 1 : (1 - rate) / (1 - disappearRate);
-  return `rgba(${r},${g},${b},${opacity})`;
+  const o = (1 - rate) / (1 - disappearRate);
+  return `rgba(${r},${g},${b},${o})`;
 };
 
 type WavesState = { line: number; params: WaveParams[]; fill: string };
@@ -74,7 +82,7 @@ const initialWavesState = (endLine: number): WavesState[] => {
 
 function App() {
   const { width, height } = useWindowSize();
-  const endLine = height * 0.7;
+  const endLine = height * 0.8;
   const [times, setTimes] = useState<[number, number]>([performance.now(), performance.now()]);
   const [wavesState, setWavesState] = useState<WavesState[]>(initialWavesState(endLine));
 
@@ -88,7 +96,7 @@ function App() {
     setWavesState((state) => {
       let needSwap = false;
       const waves = state.map((w) => {
-        const line = (w.line + elapsed / 15) % endLine;
+        const line = (w.line + elapsed / 25) % endLine;
         if (line < w.line) needSwap = true;
         const params = line < w.line ? generateWaveParams() : w.params;
         const fill = calcFill(line, endLine);
